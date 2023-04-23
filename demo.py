3a0ea6c2321255b3c3c8c49191e8879275ca2a59
paulbli <paulbli@tencent.com>
2023-04-23 17:55:50 +0800

    fix no sounds bug

--- demo.py
+++ demo.py
@@ -1,10 +1,10 @@
 import numpy as np
 import scipy.io.wavfile as wav
 import librosa
-import os,sys,shutil,argparse,copy,pickle
-import math,scipy
+import os, sys, shutil, argparse, copy, pickle
+import math, scipy
 from faceformer import Faceformer
-from transformers import Wav2Vec2FeatureExtractor,Wav2Vec2Processor
+from transformers import Wav2Vec2FeatureExtractor, Wav2Vec2Processor
 
 import torch
 import torch.nn as nn
@@ -13,17 +13,19 @@ import torch.nn.functional as F
 import cv2
 import tempfile
 from subprocess import call
-os.environ['PYOPENGL_PLATFORM'] = 'osmesa' # egl
+
+os.environ['PYOPENGL_PLATFORM'] = 'osmesa'  # egl
 import pyrender
 from psbody.mesh import Mesh
 import trimesh
+
 
 @torch.no_grad()
 def test_model(args):
     if not os.path.exists(args.result_path):
         os.makedirs(args.result_path)
 
-    #build model
+    # build model
     model = Faceformer(args)
     model.load_state_dict(torch.load(os.path.join(args.dataset, '{}.pth'.format(args.model_name))))
     model = model.to(torch.device(args.device))
@@ -31,36 +33,37 @@ def test_model(args):
 
     template_file = os.path.join(args.dataset, args.template_path)
     with open(template_file, 'rb') as fin:
-        templates = pickle.load(fin,encoding='latin1')
+        templates = pickle.load(fin, encoding='latin1')
 
     train_subjects_list = [i for i in args.train_subjects.split(" ")]
 
     one_hot_labels = np.eye(len(train_subjects_list))
     iter = train_subjects_list.index(args.condition)
     one_hot = one_hot_labels[iter]
-    one_hot = np.reshape(one_hot,(-1,one_hot.shape[0]))
+    one_hot = np.reshape(one_hot, (-1, one_hot.shape[0]))
     one_hot = torch.FloatTensor(one_hot).to(device=args.device)
 
     temp = templates[args.subject]
-             
+
     template = temp.reshape((-1))
-    template = np.reshape(template,(-1,template.shape[0]))
+    template = np.reshape(template, (-1, template.shape[0]))
     template = torch.FloatTensor(template).to(device=args.device)
 
     wav_path = args.wav_path
     test_name = os.path.basename(wav_path).split(".")[0]
     speech_array, sampling_rate = librosa.load(os.path.join(wav_path), sr=16000)
     processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")
-    audio_feature = np.squeeze(processor(speech_array,sampling_rate=16000).input_values)
-    audio_feature = np.reshape(audio_feature,(-1,audio_feature.shape[0]))
+    audio_feature = np.squeeze(processor(speech_array, sampling_rate=16000).input_values)
+    audio_feature = np.reshape(audio_feature, (-1, audio_feature.shape[0]))
     audio_feature = torch.FloatTensor(audio_feature).to(device=args.device)
 
     prediction = model.predict(audio_feature, template, one_hot)
-    prediction = prediction.squeeze() # (seq_len, V*3)
+    prediction = prediction.squeeze()  # (seq_len, V*3)
     np.save(os.path.join(args.result_path, test_name), prediction.detach().cpu().numpy())
 
+
 # The implementation of rendering is borrowed from VOCA: https://github.com/TimoBolkart/voca/blob/master/utils/rendering.py
-def render_mesh_helper(args,mesh, t_center, rot=np.zeros(3), tex_img=None, z_offset=0):
+def render_mesh_helper(args, mesh, t_center, rot=np.zeros(3), tex_img=None, z_offset=0):
     if args.dataset == "BIWI":
         camera_params = {'c': np.array([400, 400]),
                          'k': np.array([-0.19816071, 0.92822711, 0, 0, 0]),
@@ -73,59 +76,59 @@ def render_mesh_helper(args,mesh, t_center, rot=np.zeros(3), tex_img=None, z_off
     frustum = {'near': 0.01, 'far': 3.0, 'height': 800, 'width': 800}
 
     mesh_copy = Mesh(mesh.v, mesh.f)
-    mesh_copy.v[:] = cv2.Rodrigues(rot)[0].dot((mesh_copy.v-t_center).T).T+t_center
+    mesh_copy.v[:] = cv2.Rodrigues(rot)[0].dot((mesh_copy.v - t_center).T).T + t_center
     intensity = 2.0
     rgb_per_v = None
 
     primitive_material = pyrender.material.MetallicRoughnessMaterial(
-                alphaMode='BLEND',
-                baseColorFactor=[0.3, 0.3, 0.3, 1.0],
-                metallicFactor=0.8, 
-                roughnessFactor=0.8 
-            )
+        alphaMode='BLEND',
+        baseColorFactor=[0.3, 0.3, 0.3, 1.0],
+        metallicFactor=0.8,
+        roughnessFactor=0.8
+    )
 
     tri_mesh = trimesh.Trimesh(vertices=mesh_copy.v, faces=mesh_copy.f, vertex_colors=rgb_per_v)
-    render_mesh = pyrender.Mesh.from_trimesh(tri_mesh, material=primitive_material,smooth=True)
+    render_mesh = pyrender.Mesh.from_trimesh(tri_mesh, material=primitive_material, smooth=True)
 
     if args.background_black:
         scene = pyrender.Scene(ambient_light=[.2, .2, .2], bg_color=[0, 0, 0])
     else:
         scene = pyrender.Scene(ambient_light=[.2, .2, .2], bg_color=[255, 255, 255])
     camera = pyrender.IntrinsicsCamera(fx=camera_params['f'][0],
-                                      fy=camera_params['f'][1],
-                                      cx=camera_params['c'][0],
-                                      cy=camera_params['c'][1],
-                                      znear=frustum['near'],
-                                      zfar=frustum['far'])
+                                       fy=camera_params['f'][1],
+                                       cx=camera_params['c'][0],
+                                       cy=camera_params['c'][1],
+                                       znear=frustum['near'],
+                                       zfar=frustum['far'])
 
     scene.add(render_mesh, pose=np.eye(4))
 
     camera_pose = np.eye(4)
-    camera_pose[:3,3] = np.array([0, 0, 1.0-z_offset])
+    camera_pose[:3, 3] = np.array([0, 0, 1.0 - z_offset])
     scene.add(camera, pose=[[1, 0, 0, 0],
                             [0, 1, 0, 0],
                             [0, 0, 1, 1],
                             [0, 0, 0, 1]])
 
     angle = np.pi / 6.0
-    pos = camera_pose[:3,3]
+    pos = camera_pose[:3, 3]
     light_color = np.array([1., 1., 1.])
     light = pyrender.DirectionalLight(color=light_color, intensity=intensity)
 
     light_pose = np.eye(4)
-    light_pose[:3,3] = pos
-    scene.add(light, pose=light_pose.copy())
-    
-    light_pose[:3,3] = cv2.Rodrigues(np.array([angle, 0, 0]))[0].dot(pos)
+    light_pose[:3, 3] = pos
     scene.add(light, pose=light_pose.copy())
 
-    light_pose[:3,3] =  cv2.Rodrigues(np.array([-angle, 0, 0]))[0].dot(pos)
+    light_pose[:3, 3] = cv2.Rodrigues(np.array([angle, 0, 0]))[0].dot(pos)
     scene.add(light, pose=light_pose.copy())
 
-    light_pose[:3,3] = cv2.Rodrigues(np.array([0, -angle, 0]))[0].dot(pos)
+    light_pose[:3, 3] = cv2.Rodrigues(np.array([-angle, 0, 0]))[0].dot(pos)
     scene.add(light, pose=light_pose.copy())
 
-    light_pose[:3,3] = cv2.Rodrigues(np.array([0, angle, 0]))[0].dot(pos)
+    light_pose[:3, 3] = cv2.Rodrigues(np.array([0, -angle, 0]))[0].dot(pos)
+    scene.add(light, pose=light_pose.copy())
+
+    light_pose[:3, 3] = cv2.Rodrigues(np.array([0, angle, 0]))[0].dot(pos)
     scene.add(light, pose=light_pose.copy())
 
     flags = pyrender.RenderFlags.SKIP_CULL_FACES
@@ -138,20 +141,21 @@ def render_mesh_helper(args,mesh, t_center, rot=np.zeros(3), tex_img=None, z_off
 
     return color[..., ::-1]
 
+
 def render_sequence(args):
     wav_path = args.wav_path
     test_name = os.path.basename(wav_path).split(".")[0]
-    predicted_vertices_path = os.path.join(args.result_path,test_name+".npy")
+    predicted_vertices_path = os.path.join(args.result_path, test_name + ".npy")
     if args.dataset == "BIWI":
         template_file = os.path.join(args.dataset, args.render_template_path, "BIWI.ply")
     elif args.dataset == "vocaset":
         template_file = os.path.join(args.dataset, args.render_template_path, "FLAME_sample.ply")
-         
+
     print("rendering: ", test_name)
-                 
+
     template = Mesh(filename=template_file)
     predicted_vertices = np.load(predicted_vertices_path)
-    predicted_vertices = np.reshape(predicted_vertices,(-1,args.vertice_dim//3,3))
+    predicted_vertices = np.reshape(predicted_vertices, (-1, args.vertice_dim // 3, 3))
 
     output_path = args.output_path
     if not os.path.exists(output_path):
@@ -159,23 +163,24 @@ def render_sequence(args):
 
     num_frames = predicted_vertices.shape[0]
     tmp_video_file = tempfile.NamedTemporaryFile('w', suffix='.mp4', dir=output_path)
-    
+
     writer = cv2.VideoWriter(tmp_video_file.name, cv2.VideoWriter_fourcc(*'mp4v'), args.fps, (800, 800), True)
     center = np.mean(predicted_vertices[0], axis=0)
 
     for i_frame in range(num_frames):
         render_mesh = Mesh(predicted_vertices[i_frame], template.f)
-        pred_img = render_mesh_helper(args,render_mesh, center)
+        pred_img = render_mesh_helper(args, render_mesh, center)
         pred_img = pred_img.astype(np.uint8)
         writer.write(pred_img)
 
     writer.release()
-    file_name = test_name+"_"+args.subject+"_condition_"+args.condition
+    file_name = test_name + "_" + args.subject + "_condition_" + args.condition
 
-    video_fname = os.path.join(output_path, file_name+'.mp4')
-    cmd = ('ffmpeg' + ' -i {0} -pix_fmt yuv420p -qscale 0 {1}'.format(
-       tmp_video_file.name, video_fname)).split()
+    video_fname = os.path.join(output_path, file_name + '.mp4')
+    cmd = ('ffmpeg' + ' -i {0} -i {1} -pix_fmt yuv420p -qscale 0 -vcodec copy -acodec aac {2} '.format(
+        tmp_video_file.name, wav_path, video_fname)).split()
     call(cmd)
+
 
 def main():
     parser = argparse.ArgumentParser(description='FaceFormer: Speech-Driven 3D Facial Animation with Transformers')
@@ -184,7 +189,8 @@ def main():
     parser.add_argument("--fps", type=float, default=25, help='frame rate - 30 for vocaset; 25 for BIWI')
     parser.add_argument("--feature_dim", type=int, default=128, help='64 for vocaset; 128 for BIWI')
     parser.add_argument("--period", type=int, default=25, help='period in PPE - 30 for vocaset; 25 for BIWI')
-    parser.add_argument("--vertice_dim", type=int, default=23370*3, help='number of vertices - 5023*3 for vocaset; 23370*3 for BIWI')
+    parser.add_argument("--vertice_dim", type=int, default=23370 * 3,
+                        help='number of vertices - 5023*3 for vocaset; 23370*3 for BIWI')
     parser.add_argument("--device", type=str, default="cuda")
     parser.add_argument("--train_subjects", type=str, default="F2 F3 F4 M3 M4 M5")
     parser.add_argument("--test_subjects", type=str, default="F1 F5 F6 F7 F8 M1 M2 M6")
@@ -192,14 +198,17 @@ def main():
     parser.add_argument("--wav_path", type=str, default="demo/wav/test.wav", help='path of the input audio signal')
     parser.add_argument("--result_path", type=str, default="demo/result", help='path of the predictions')
     parser.add_argument("--condition", type=str, default="M3", help='select a conditioning subject from train_subjects')
-    parser.add_argument("--subject", type=str, default="M1", help='select a subject from test_subjects or train_subjects')
+    parser.add_argument("--subject", type=str, default="M1",
+                        help='select a subject from test_subjects or train_subjects')
     parser.add_argument("--background_black", type=bool, default=True, help='whether to use black background')
     parser.add_argument("--template_path", type=str, default="templates.pkl", help='path of the personalized templates')
-    parser.add_argument("--render_template_path", type=str, default="templates", help='path of the mesh in BIWI/FLAME topology')
-    args = parser.parse_args()   
+    parser.add_argument("--render_template_path", type=str, default="templates",
+                        help='path of the mesh in BIWI/FLAME topology')
+    args = parser.parse_args()
 
     test_model(args)
     render_sequence(args)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()

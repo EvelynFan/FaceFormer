bd91f7028e06ed8abac4b6dfc45d9ecdbe690869
paulbli <paulbli@tencent.com>
2023-04-25 18:45:41 +0800

    add head pose and eye blink feature

--- demo.py
+++ demo.py
@@ -9,10 +9,12 @@ from transformers import Wav2Vec2FeatureExtractor, Wav2Vec2Processor
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import random
 
 import cv2
 import tempfile
 from subprocess import call
+from smpl_webuser.serialization import load_model
 
 os.environ['PYOPENGL_PLATFORM'] = 'osmesa'  # egl
 import pyrender
@@ -59,7 +61,111 @@ def test_model(args):
 
     prediction = model.predict(audio_feature, template, one_hot)
     prediction = prediction.squeeze()  # (seq_len, V*3)
-    np.save(os.path.join(args.result_path, test_name), prediction.detach().cpu().numpy())
+    predicted_vertices = prediction.detach().cpu().numpy()
+    flame_model_fname = "./flame/generic_model.pkl"
+    predicted_vertices_frames = predicted_vertices.shape[0]
+    predicted_vertices = add_eye_blink_sequences(predicted_vertices, flame_model_fname)
+    print(predicted_vertices.shape)
+    predicted_vertices = np.reshape(predicted_vertices, (predicted_vertices_frames, 15069))
+    predicted_vertices = add_head_pose_sequences(predicted_vertices, flame_model_fname)
+    np.save(os.path.join(args.result_path, test_name), predicted_vertices)
+
+
+def add_eye_blink_sequences(predicted_vertices_old, flame_model_fname, num_blinks=2, blink_duration=15, ):
+    # Load sequence files
+    num_frames = predicted_vertices_old.shape[0]
+    if num_frames == 0:
+        print('No sequence meshes found')
+        return
+
+    # Load FLAME head model
+    model = load_model(flame_model_fname)
+
+    blink_exp_betas = np.array(
+        [0.04676158497927314, 0.03758675711005459, -0.8504121184951298, 0.10082324210507627, -0.574142329926028,
+         0.6440016589938355, 0.36403779939335984, 0.21642312586261656, 0.6754551784690193, 1.80958618462892,
+         0.7790133813372259, -0.24181691256476057, 0.826280685961679, -0.013525679499256753, 1.849393698014113,
+         -0.263035686247264, 0.42284248271332153, 0.10550891351425384, 0.6720993875023772, 0.41703592560736436,
+         3.308019065485072, 1.3358509602858895, 1.2997143108969278, -1.2463587328652894, -1.4818961382824924,
+         -0.6233880069345369, 0.26812528424728455, 0.5154889093160832, 0.6116267181402183, 0.9068826814583771,
+         -0.38869613253448576, 1.3311776710005476, -0.5802565274559162, -0.7920775624092143, -1.3278601781150017,
+         -1.2066425872386706, 0.34250140710360893, -0.7230686724732668, -0.6859285483325263, -1.524877347586566,
+         -1.2639479212965923, -0.019294228307535275, 0.2906175769381998, -1.4082782880837976, 0.9095436721066045,
+         1.6007365724960054, 2.0302381182163574, 0.5367600947801505, -0.12233184771794232, -0.506024823810769,
+         2.4312326730634783, 0.5622323258974669, 0.19022395712837198, -0.7729758559103581, -1.5624233513002923,
+         0.8275863297957926, 1.1661887586553132, 1.2299311381779416, -1.4146929897142397, -0.42980549225554004,
+         -1.4282801579740614, 0.26172301287347266, -0.5109318114918897, -0.6399495909195524, -0.733476856285442,
+         1.219652074726591, 0.08194907995352405, 0.4420398361785991, -1.184769973221183, 1.5126082924326332,
+         0.4442281271081217, -0.005079477284341147, 1.764084274265486, 0.2815940264026848, 0.2898827213634057,
+         -0.3686662696397026, 1.9125365942683656, 2.1801452989500274, -2.3915065327980467, 0.5794919897154226,
+         -1.777680085517591, 2.9015718628823604, -2.0516886588315777, 0.4146899057365943, -0.29917763685660903,
+         -0.5839240983516372, 2.1592457102697007, -0.8747902386178202, -0.5152943072876817, 0.12620001057735733,
+         1.3144109838803493, -0.5027032013330108, 1.2160353388774487, 0.7543834001473375, -3.512095548974531,
+         -0.9304382646186183, -0.30102930208709433, 0.9332135959962723, -0.52926196689098, 0.23509772959302958])
+
+    step = blink_duration // 3
+    blink_weights = np.hstack(
+        (np.interp(np.arange(step), [0, step], [0, 1]), np.ones(step), np.interp(np.arange(step), [0, step], [1, 0])))
+
+    frequency = num_frames // (num_blinks + 1)
+    weights = np.zeros(num_frames)
+    for i in range(num_blinks):
+        x1 = (i + 1) * frequency - blink_duration // 2
+        x2 = x1 + 3 * step
+        if x1 >= 0 and x2 < weights.shape[0]:
+            weights[x1:x2] = blink_weights
+
+    predicted_vertices = np.zeros((num_frames, model.v_template.shape[0], model.v_template.shape[1]))
+
+    for frame_idx in range(num_frames):
+        model.v_template[:] = Mesh(np.reshape(predicted_vertices_old[frame_idx], (5023, 3))).v
+        model.betas[300:] = weights[frame_idx] * blink_exp_betas
+        predicted_vertices[frame_idx] = model.r
+    return predicted_vertices
+
+
+def add_head_pose_sequences(predicted_vertices_old, flame_model_fname, pose_frame=70, pose_idx=2,
+                            rot_angle=np.pi / 60):
+    # Load sequence files
+    num_frames = predicted_vertices_old.shape[0]
+    if num_frames == 0:
+        print('No sequence meshes found')
+        return
+    # Load FLAME head model
+    pose_frame_tuple = []
+    model = load_model(flame_model_fname)
+    model_parms = np.zeros((num_frames, model.pose.shape[0]))
+    # Generate interpolated pose parameters for each frame
+    pose_frame_tuple.extend((0,) * (num_frames % pose_frame))
+    pose_frames = int(num_frames / pose_frame)
+    for i in range(pose_frames):
+        angle_range = random.randint(0, 10)
+        if angle_range <= 3:
+            left_rot_angle = np.pi / 180
+            right_rot_angle = np.pi / 180
+        else:
+            left_rot_angle = np.pi / (random.randint(160, 180))
+            right_rot_angle = np.pi / (random.randint(160, 180))
+        x1, y1 = [0, pose_frame // 4], [0, left_rot_angle]
+        x2, y2 = [pose_frame // 4, pose_frame // 2], [left_rot_angle, 0]
+        x3, y3 = [pose_frame // 2, 3 * pose_frame // 4], [0, -right_rot_angle]
+        x4, y4 = [3 * pose_frame // 4, pose_frame], [-right_rot_angle, 0]
+        xsteps1 = np.arange(0, pose_frame // 4)
+        xsteps2 = np.arange(pose_frame // 4, pose_frame // 2)
+        xsteps3 = np.arange(pose_frame // 2, 3 * pose_frame // 4)
+        xsteps4 = np.arange(3 * pose_frame // 4, pose_frame)
+        pose_frame_tuple.extend((np.interp(xsteps1, x1, y1),
+                                 np.interp(xsteps2, x2, y2),
+                                 np.interp(xsteps3, x3, y3),
+                                 np.interp(xsteps4, x4, y4)))
+    model_parms[:, pose_idx] = np.hstack(pose_frame_tuple)
+
+    predicted_vertices = np.zeros((num_frames, model.v_template.shape[0], model.v_template.shape[1]))
+    for frame_idx in range(num_frames):
+        model.v_template[:] = Mesh(np.reshape(predicted_vertices_old[frame_idx], (5023, 3))).v
+        model.pose[:] = model_parms[frame_idx]
+        predicted_vertices[frame_idx] = model.r
+    return predicted_vertices
 
 
 # The implementation of rendering is borrowed from VOCA: https://github.com/TimoBolkart/voca/blob/master/utils/rendering.py
